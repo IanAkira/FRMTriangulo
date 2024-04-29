f35c50bb0621c8ea2a6d6b5ba103efb8ced2471e
1072417788 <1072417788@UNS07DT106651.animaedu.intranet>
2024-04-29 09:44:47 -0300

    é foda

---
+++ src/main/java/main/Principal.java
@@ -0,0 +1,14 @@
+package main;
+
+import visao.frmTriangulo;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        frmTriangulo janela = new frmTriangulo();
+        janela.setVisible(true);
+        
+    }
+
+}
